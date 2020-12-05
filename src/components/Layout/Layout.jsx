7262e79485b3515e1b36eb527e150e980449900d
zephyrus21 <pandey.piyus@gmail.com>
2020-12-05 16:26:37 +0530

    adding styles to Auth page

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import NavBar from '../NavBar/NavBar';
-
 const Layout = (props) => {
   return (
     <>
-      <NavBar />
       <main>{props.children}</main>
     </>
   );
