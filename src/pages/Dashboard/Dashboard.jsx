d14bb91fda4ac8a4ff8a0ed4c2d7b22731ead688
zephyrus21 <pandey.piyus@gmail.com>
2020-12-08 10:50:32 +0530

    adding Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-
 import NavBar from '../../components/NavBar/NavBar';
+
+import heroRight from '../../Assets/hero-right.svg';
+import fig1 from '../../Assets/fig-1.svg';
+import './Dashboard.css';
 
 const Dashboard = () => {
   return (
-    <div>
+    <section className="dashboard">
       <NavBar />
-    </div>
+      <div className="hero">
+        <div className="hero-left"></div>
+        <div className="hero-right">
+          <img className="hero-right-back" src={heroRight} alt="hero-right" />
+          <img className="hero-right-front" src={fig1} alt="hero-right" />
+        </div>
+      </div>
+    </section>
   );
 };
 
