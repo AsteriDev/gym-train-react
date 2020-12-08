bdef03b0d82d9fda8784c4d60ae2c4a8b26a450f
zephyrus21 <pandey.piyus@gmail.com>
2020-12-08 17:11:07 +0530

    Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import NavBar from '../../components/NavBar/NavBar';
 
 import heroRight from '../../Assets/hero-right.svg';
 import fig1 from '../../Assets/fig-1.svg';
+import fig2 from '../../Assets/fig-2.svg';
 import './Dashboard.css';
 
 const Dashboard = () => {
@@ -17,8 +18,15 @@ const Dashboard = () => {
           <img className="hero-right-front" src={fig1} alt="hero-right" />
         </div>
       </section>
-
-      <section></section>
+      <section className="course">
+        <div className="course-main">
+          <img className="course-main-fig" src={fig2} alt="" />
+          <div className="course-main-content">
+            <h1>COURSES ENROLLED</h1>
+            <div>Course</div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
