59190bb111629286ff047a9a2d57cf6b59160486
zephyrus21 <pandey.piyus@gmail.com>
2020-12-25 12:12:32 +0530

    dash

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -10,8 +10,8 @@ import Footer from '../../components/Footer/Footer';
 
 import heroRight from '../../Assets/hero-right.svg';
 import fig1 from '../../Assets/fig-1.svg';
-import fig2 from '../../Assets/fig-2.svg';
 import fb from '../../Assets/Fb-com.svg';
+import plus from '../../Assets/plus.svg';
 import './Dashboard.css';
 
 const Dashboard = () => {
@@ -57,77 +57,76 @@ const Dashboard = () => {
       </section>
       <section className="course">
         <div className="course-main">
-          <div className="course-container">
-            <div className="course-main-content">
-              <h1>COURSES ENROLLED</h1>
-              <div className="card-container">
-                <Link className="card-link" to={'/course'}>
-                  <Card cardType="small">
-                    <img
-                      className="card-image"
-                      src="https://images.unsplash.com/photo-1576678927484-cc907957088c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                      alt=""
-                    />
-                    <p className="card-title">30 day fitness</p>
-                    <p className="card-description">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Reiciendis fugit culpa aliquid amet necessitatibus
-                      deleniti ipsa ipsum expedita.
-                    </p>
-                  </Card>
-                </Link>
-                <Link className="card-link" to={'/course'}>
-                  <Card cardType="small">
-                    <img
-                      className="card-image"
-                      src="https://images.unsplash.com/photo-1576678927484-cc907957088c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                      alt=""
-                    />
-                    <p className="card-title">30 day fitness</p>
-                    <p className="card-description">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Reiciendis fugit culpa aliquid amet necessitatibus
-                      deleniti ipsa ipsum expedita.
-                    </p>
-                  </Card>
-                </Link>
-              </div>
+          <div className="course-main-content">
+            <h1>COURSES ENROLLED</h1>
+            <div className="card-container">
+              <Link className="card-link" to={'/course'}>
+                <Card cardType="small">
+                  <img
+                    className="card-image"
+                    src="https://images.unsplash.com/photo-1576678927484-cc907957088c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+                    alt=""
+                  />
+                  <p className="card-title">30 day fitness</p>
+                  <p className="card-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Reiciendis fugit culpa aliquid amet necessitatibus deleniti
+                    ipsa ipsum expedita.
+                  </p>
+                </Card>
+              </Link>
+              <Link className="card-link" to={'/course'}>
+                <Card cardType="small">
+                  <img
+                    className="card-image"
+                    src="https://images.unsplash.com/photo-1576678927484-cc907957088c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+                    alt=""
+                  />
+                  <p className="card-title">30 day fitness</p>
+                  <p className="card-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Reiciendis fugit culpa aliquid amet necessitatibus deleniti
+                    ipsa ipsum expedita.
+                  </p>
+                </Card>
+              </Link>
             </div>
-            <div className="course-bonus">
-              <h1>BONUS COURSES</h1>
-              <div className="card-container">
-                <Link className="card-link" to={'/course'}>
-                  <Card cardType="small">
-                    <img
-                      className="card-image"
-                      src="https://images.unsplash.com/photo-1576678927484-cc907957088c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                      alt=""
-                    />
-                    <p className="card-title">30 day fitness</p>
-                    <p className="card-description">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Reiciendis fugit culpa aliquid amet necessitatibus
-                      deleniti ipsa ipsum expedita.
-                    </p>
-                  </Card>
-                </Link>
-                <Link className="card-link" to={'/course'}>
-                  <Card cardType="small">
-                    <img
-                      className="card-image"
-                      src="https://images.unsplash.com/photo-1576678927484-cc907957088c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                      alt=""
-                    />
-                    <p className="card-title">30 day fitness</p>
-                    <p className="card-description">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Reiciendis fugit culpa aliquid amet necessitatibus
-                      deleniti ipsa ipsum expedita.
-                    </p>
-                  </Card>
-                </Link>
-              </div>
-            </div>
+          </div>
+        </div>
+        <img src={plus} alt="" />
+        <div className="course-bonus">
+          <h1>BONUS COURSES</h1>
+          <div className="card-container">
+            <Link className="card-link" to={'/course'}>
+              <Card cardType="small">
+                <img
+                  className="card-image"
+                  src="https://images.unsplash.com/photo-1576678927484-cc907957088c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+                  alt=""
+                />
+                <p className="card-title">30 day fitness</p>
+                <p className="card-description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis fugit culpa aliquid amet necessitatibus deleniti
+                  ipsa ipsum expedita.
+                </p>
+              </Card>
+            </Link>
+            <Link className="card-link" to={'/course'}>
+              <Card cardType="small">
+                <img
+                  className="card-image"
+                  src="https://images.unsplash.com/photo-1576678927484-cc907957088c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+                  alt=""
+                />
+                <p className="card-title">30 day fitness</p>
+                <p className="card-description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis fugit culpa aliquid amet necessitatibus deleniti
+                  ipsa ipsum expedita.
+                </p>
+              </Card>
+            </Link>
           </div>
         </div>
       </section>
