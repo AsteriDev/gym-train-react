acd2caa03b5c948ab64db97d3307b87161250335
zephyrus21 <pandey.piyus@gmail.com>
2021-01-07 00:42:31 +0530

    home init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, withRouter } from 'react-router-dom';
 
 import { AuthContextProvider } from './context/AuthContext';
 import './App.css';
+import Home from './pages/Home/Home';
 import Auth from './pages/Auth/Auth';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Course from './pages/Course/Course';
@@ -10,7 +11,8 @@ import Course from './pages/Course/Course';
 function App() {
   return (
     <AuthContextProvider>
-      <Route path="/" exact component={Auth} />
+      <Route path="/" exact component={Home} />
+      <Route path="/auth" exact component={Auth} />
       <Route path="/dashboard" component={Dashboard} />
       <Route path="/course" component={Course} />
     </AuthContextProvider>
