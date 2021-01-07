5553b1fd9cb0e4721bbee7e03ef88b993b1e77e2
zephyrus21 <pandey.piyus@gmail.com>
2021-01-08 01:23:48 +0530

    adding navbar in homw

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './Home.css';
 
 const Home = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const clickHandler = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <>
-      <nav>Navbar</nav>
+      <nav class="home-nav">
+        <NavLink class="home-nav-logo" to="/">
+          LOGO
+        </NavLink>
+        <div className="home-nav-icon" onClick={clickHandler}>
+          <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+        <div className={clicked ? 'home-nav-menu active' : 'home-nav-menu'}>
+          <NavLink className="home-nav-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="home-nav-link" to="/">
+            About Us
+          </NavLink>
+          <NavLink className="home-nav-link" to="/">
+            Contact Us
+          </NavLink>
+          <NavLink className="home-nav-login" to="/auth">
+            Login
+          </NavLink>
+        </div>
+      </nav>
       <header>Header</header>
     </>
   );
